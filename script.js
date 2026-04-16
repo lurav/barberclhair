fa6fc0baac3e568f8004c303998e73ed75d2421d
lurav <dj.lr.events@gmail.com>
2026-04-16 13:01:48 +0200

    Horaires dynamiques selon le jour

--- script.js
+++ script.js
@@ -172,7 +172,20 @@ const renderTimeSlots = async () => {
     } catch(e) { console.log("Mode hors-ligne ou Vercel non configuré"); }
 
     timeSlotsGrid.innerHTML = "";
-    const slots = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"];
+    
+    const selectedDateObj = new Date(hiddenDateInput.value);
+    const dayOfWeek = selectedDateObj.getDay();
+    
+    let slots = [];
+    if (dayOfWeek >= 2 && dayOfWeek <= 4) { // Mar - Jeu (09:00 - 19:00)
+        slots = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30"];
+    } else if (dayOfWeek === 5) { // Vendredi (09:00-12:30 / 14:00-19:00)
+        slots = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30"];
+    } else if (dayOfWeek === 6) { // Samedi (09:00 - 19:00)
+        slots = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30"];
+    } else if (dayOfWeek === 0) { // Dimanche (10:00 - 14:00)
+        slots = ["10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30"];
+    }
     
     slots.forEach(time => {
         const slotDiv = document.createElement("div");
